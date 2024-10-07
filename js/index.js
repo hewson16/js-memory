7b603d2eb9680c97020832ffc87e84d188775008
phewson16 <peterhewson16@gmail.com>
2024-10-07 10:20:19 -0400

    name and age

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+// define a name and show it on the screen
+const name = 'Hermione';
+const nameElement = document.getElementById('name');
+nameElement.textContent = name;
+
+//define an age and show that on the screen
+const age = 11;
+const ageElement = document.getElementById('age');
+ageElement.textContent = age;
+
+
+// create css classes for is good/not
+// show the right word/class styles on the screen depending
+
